93cc175a1ba724f93359614058f24e1eaca5933b
Lealdade1Lugar <iltoneto400@gmail.com>
2024-09-01 16:32:10 -0300

    criando funções para ajudar o usuario

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <ctype.h>
 
     using namespace std;
     const int quant_palavras = 5;
@@ -21,9 +23,10 @@
         return rand() % (quant_palavras);
     }
 
-    void imprimePalavra(int palavra) {
-        for(int i = 0; i < 30; i++){
-            cout <<  banco[palavra][i];
+    void imprimePalavraDigitada() {
+        cout << "\n     "<<strlen(palavraSort)<<" letras \n\n    ";
+        for(int i = 0; i < strlen(palavraSort); i++){
+            cout <<  palavraDigitada[i] << " ";
         }
     }
 
@@ -34,77 +37,133 @@
     }
 
     void limpaPalavraDigitadas(){
-        for(int i = 0; i< 30; i++){
+        for(int i = 0; i< strlen(palavraSort); i++){
             palavraDigitada[i] = '_';
         }
     }
 
     void imprimeForca(int letrasErradas) {
         switch(letrasErradas){
+         case 0:
+            cout<<"  _____ "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            break;
         case 1:
-            cout<<"  ______";
-            cout<<" |";
-            cout<<" |";
-            cout<<" |";
-            cout<<" |";
-                break;
-
+            cout<<"  _____ "<<endl;
+            cout<<" |    O "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            break;
         case 2:
-            cout<<"  ______";
-            cout<<" |     O ";
-            cout<<" |      \\";
-            cout<<" |";
-            cout<<" |";
-                break;
-
+            cout<<"  _____ "<<endl;
+            cout<<" |    O "<<endl;
+            cout<<" |    | "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            break;
         case 3:
-            cout<<"  ______";
-            cout<<" |     O ";
-            cout<<" |     |\\";
-            cout<<" |";
-            cout<<" |";
-                break;
-
+            cout<<"  _____ "<<endl;
+            cout<<" |    O "<<endl;
+            cout<<" |   /| "<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            break;
         case 4:
-            cout<<"  ______";
-            cout<<" |     O ";
-            cout<<" |    /|\\";
-            cout<<" |";
-            cout<<" |";
-                break;
-
+            cout<<"  _____ "<<endl;
+            cout<<" |    O "<<endl;
+            cout<<" |   /|\\"<<endl;
+            cout<<" |      "<<endl;
+            cout<<" |      "<<endl;
+            break;
         case 5:
-            cout<<"  ______";
-            cout<<" |     O ";
-            cout<<" |    /|\\";
-            cout<<" |      \\";
-            cout<<" |       ";
-
-                break;
+            cout<<"  _____ "<<endl;
+            cout<<" |    O "<<endl;
+            cout<<" |   /|\\"<<endl;
+            cout<<" |   /  "<<endl;
+            cout<<" |      "<<endl;
+            break;
         case 6:
-            cout<<"  ______";
-            cout<<" |     O ";
-            cout<<" |    /|\\";
-            cout<<" |    / \\";
-            cout<<" |       ";
-                break;
-
+            cout<<"  _____ "<<endl;
+            cout<<" |    O "<<endl;
+            cout<<" |   /|\\"<<endl;
+            cout<<" |   / \\"<<endl;
+            cout<<" |       "<<endl;
+            break;
         }
     }
 
+    bool verificaLetra(char letra, char vetor[26]) {
+        for(int i = 0; i<strlen(vetor); i++) {
+            if (letra == vetor[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void substitui (char letra) {
+        for(int i = 0; i<strlen(palavraDigitada); i++) {
+            if (palavraSort[i] == letra){
+                palavraDigitada[i] = letra;
+            }
+        }
+    }
+
+    void imprimeLetrasDigitadas(char letras[26], int tamanho) {
+        cout << "\n\nLetras digitdas: ";
+        for(int i = 0;i < tamanho; i++){
+            cout << letras[i] << ", ";
+        }
+        cout << "\n\n";
+    }
+
     int main() {
+
+
+        int indicePalavra = sorteia(); // sortear uma palavra
+
+        copia(indicePalavra);
+
         limpaPalavraDigitadas(); // função para limpar vetor da palavra digitada
 
+        int contadorLetrasDigitadas = 0; // contador de letras digitadas
 
-        contadorLetrasDigitadas = 0; // contador de letras digitadas
-        int indicePalavra = sorteia(); // sortear uma palavra
 
         char letrasDigitadas[26];
         int contadorLetras = 0;
+        int contadorErros = 0;
+        char letra;
 
-        while(contadorLetras <= 6){
+        while(contadorErros <= 6){
+            system("cls");
+            imprimeForca(contadorErros);
+            imprimePalavraDigitada();
+            imprimeLetrasDigitadas(letrasDigitadas, contadorLetrasDigitadas);
+            cout << "\n\n Tentativas restantes: " << 6-contadorErros<<"\n\n";
+            cout << "\n\n";
+            cout << "Digite uma letra: ";
+            cin >> letra;
+            letra = tolower(letra);
+            if(verificaLetra(letra, letrasDigitadas)){
+                cout << "Letra ja foi digitada! Tente novamente!\n\n";
+                system("pause");
+            }
+            else{
+                letrasDigitadas[contadorLetrasDigitadas] = letra;
+                contadorLetrasDigitadas++;
 
-
+                if(verificaLetra(letra, palavraSort)) {
+                    substitui(letra);
+                }else{
+                    cout << "\nLetra incorreta! Tente novamente!\n\n";
+                    contadorErros++;
+                    system("pause");
+                }
+            }
         }
 
 
